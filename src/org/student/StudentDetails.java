c8d2c90ecc6b3d16f7d0c40c2e1b36e68ae777f6
subbusara <sudhaparvatham@gmail.com>
2023-10-19 14:54:30 +0530

    studentrankdetails

---
+++ src/org/student/StudentDetails.java
@@ -0,0 +1,52 @@
+package org.student;
+import java.util.Scanner;
+
+public class StudentDetails {
+	public static void main(String[]args) {
+		Scanner scan=new Scanner(System.in);
+		System.out.println("Enter Student ID");
+		long studentId=scan.nextLong();
+		System.out.println("Student Id Number:"+studentId);
+		
+		System.out.println("Enter your Name");
+		String studentName=scan.next();
+		System.out.println("Student Name Is:"+studentName);
+		
+		System.out.println("Enter your Mark1");
+		byte mark1=scan.nextByte();
+		System.out.println("mark1:"+mark1);
+		
+		System.out.println("Enter your mark2");
+		byte mark2=scan.nextByte();
+		System.out.println("mark2:"+mark2);
+		
+		System.out.println("Enter your mark3");
+		byte mark3=scan.nextByte();
+		System.out.println("mark3:"+mark3);
+		
+		System.out.println("Enter your mark4");
+		byte mark4=scan.nextByte();
+		System.out.println("mark4:"+mark4);
+		
+		System.out.println("Enter your mark5");
+		byte mark5=scan.nextByte();
+		System.out.println("mark5:"+mark5);
+		//finding Total
+		
+		long total=mark1+mark2+mark3+mark4+mark5;
+		System.out.println("Total:"+total);
+		
+		//finding average
+		float average=total/5;
+		System.out.println("Average:"+average);
+				
+		
+		
+	
+		
+		
+		
+	}
+	
+
+}
